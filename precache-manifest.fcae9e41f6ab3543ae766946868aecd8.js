6cae752f5b057a9c1dca5b1a51c43c7c8977f77b
nvanny <nvanny@ucsd.edu>
2020-10-03 02:30:16 -0700

    Updates

---
+++ precache-manifest.fcae9e41f6ab3543ae766946868aecd8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e304ad69fc6d77493bc28858013b67a4",
+    "url": "http://nvanny.github.io/personal-website/index.html"
+  },
+  {
+    "revision": "9decb57c306aa7c80253",
+    "url": "http://nvanny.github.io/personal-website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cf55c2b1f8fc9d77d1a2",
+    "url": "http://nvanny.github.io/personal-website/static/css/main.b037543d.chunk.css"
+  },
+  {
+    "revision": "9decb57c306aa7c80253",
+    "url": "http://nvanny.github.io/personal-website/static/js/2.2d74aecb.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "http://nvanny.github.io/personal-website/static/js/2.2d74aecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf55c2b1f8fc9d77d1a2",
+    "url": "http://nvanny.github.io/personal-website/static/js/main.476831d9.chunk.js"
+  },
+  {
+    "revision": "ce9f1f50137e39bc02fc74fffc9ba414",
+    "url": "http://nvanny.github.io/personal-website/static/js/pdf.worker.entry.a38a17d6.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "http://nvanny.github.io/personal-website/static/js/pdf.worker.entry.a38a17d6.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a968c2f9f74f7eaf74a",
+    "url": "http://nvanny.github.io/personal-website/static/js/runtime-main.1e7679f0.js"
+  },
+  {
+    "revision": "83106c2e8078bc922c21da15d13f17cf",
+    "url": "http://nvanny.github.io/personal-website/static/media/College Resume Update.83106c2e.pdf"
+  },
+  {
+    "revision": "63ac6aa65dcf15dc0ba81d26681abaf1",
+    "url": "http://nvanny.github.io/personal-website/static/media/Football.63ac6aa6.png"
+  },
+  {
+    "revision": "208292698f5d314e3537d43cdec9fe07",
+    "url": "http://nvanny.github.io/personal-website/static/media/IMG_2669.20829269.JPG"
+  },
+  {
+    "revision": "9bbba98dc4a278a342daf528d48f778f",
+    "url": "http://nvanny.github.io/personal-website/static/media/better-shark.9bbba98d.png"
+  },
+  {
+    "revision": "951707e3a563cd6c97a5a064deaa5b30",
+    "url": "http://nvanny.github.io/personal-website/static/media/logo1 copy.951707e3.png"
+  },
+  {
+    "revision": "5fad8b1458e79ab5ca99cff29e2f6cf7",
+    "url": "http://nvanny.github.io/personal-website/static/media/red-white-marble.5fad8b14.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.dc731c7e773cd08f185fe53ba5283e7e.js"
+  "http://nvanny.github.io/personal-website/precache-manifest.fcae9e41f6ab3543ae766946868aecd8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://nvanny.github.io/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
