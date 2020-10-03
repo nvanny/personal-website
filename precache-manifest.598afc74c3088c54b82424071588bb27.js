00ec79aac5a9d939f166deea7cff099aca3f1549
nvanny <nvanny@ucsd.edu>
2020-10-03 00:31:07 -0700

    Updates

---
+++ precache-manifest.598afc74c3088c54b82424071588bb27.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b9e84028a8ba4d08adac3dec035a06a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60e66efa939c26bb5a0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3da2fb9eda4f60b606af",
+    "url": "/static/css/main.b037543d.chunk.css"
+  },
+  {
+    "revision": "d60e66efa939c26bb5a0",
+    "url": "/static/js/2.97b88c5a.chunk.js"
+  },
+  {
+    "revision": "596676716ca1328e2733d607535d6ca7",
+    "url": "/static/js/2.97b88c5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da2fb9eda4f60b606af",
+    "url": "/static/js/main.e9efc420.chunk.js"
+  },
+  {
+    "revision": "1fc292e8a471d29916097f79eb11fe6d",
+    "url": "/static/js/pdf.worker.entry.fc5ce9eb.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "/static/js/pdf.worker.entry.fc5ce9eb.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  },
+  {
+    "revision": "83106c2e8078bc922c21da15d13f17cf",
+    "url": "/static/media/College Resume Update.83106c2e.pdf"
+  },
+  {
+    "revision": "63ac6aa65dcf15dc0ba81d26681abaf1",
+    "url": "/static/media/Football.63ac6aa6.png"
+  },
+  {
+    "revision": "208292698f5d314e3537d43cdec9fe07",
+    "url": "/static/media/IMG_2669.20829269.JPG"
+  },
+  {
+    "revision": "9bbba98dc4a278a342daf528d48f778f",
+    "url": "/static/media/better-shark.9bbba98d.png"
+  },
+  {
+    "revision": "951707e3a563cd6c97a5a064deaa5b30",
+    "url": "/static/media/logo1 copy.951707e3.png"
+  },
+  {
+    "revision": "5fad8b1458e79ab5ca99cff29e2f6cf7",
+    "url": "/static/media/red-white-marble.5fad8b14.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.289bb024e0f6c6766456276846f7db5c.js"
+  "/precache-manifest.598afc74c3088c54b82424071588bb27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
